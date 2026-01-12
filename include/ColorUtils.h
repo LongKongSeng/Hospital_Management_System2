96cd2223790ba486e3b9dbf14fb534e9d92a3d12
LongKongSeng <colintehjr@gmail.com>
2026-01-12 11:57:50 +0800

    Update ColorUtils.h

--- include/ColorUtils.h
+++ include/ColorUtils.h
@@ -10,10 +10,23 @@ using namespace std;
 enum ConsoleColor {
     BLACK = 0,
     DARK_BLUE = 1,
+    DARK_GREEN = 2,
+    DARK_CYAN = 3,
+    DARK_RED = 4,
+    DARK_MAGENTA = 5,
+    DARK_YELLOW = 6,
+    GRAY = 7,
+    DARK_GRAY = 8,
     BLUE = 9,
-    LIGHT_CYAN = 11,      // Bright light blue for selected items
-    LIGHT_BLUE = 9,       // Light blue
-    CYAN = 3,             // Dark cyan
+    LIGHT_BLUE = 9,
+    GREEN = 10,
+    LIGHT_GREEN = 10,     // Added for charts
+    CYAN = 11,
+    LIGHT_CYAN = 11,
+    RED = 12,
+    LIGHT_RED = 12,       // Added for errors
+    MAGENTA = 13,
+    LIGHT_MAGENTA = 13,   // Added for yearly charts
     YELLOW = 14,
     WHITE = 15
 };
