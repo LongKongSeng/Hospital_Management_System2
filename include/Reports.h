f2cafd4e4680db04e918752057286943a3015028
LongKongSeng <colintehjr@gmail.com>
2026-01-14 18:30:09 +0800

    Update Reports.h

--- include/Reports.h
+++ include/Reports.h
@@ -3,30 +3,28 @@
 
 #include "Database.h"
 #include <iostream>
-#include <iomanip>
 #include <string>
 #include <vector>
+#include <map>
+#include <iomanip>
 
 using namespace std;
 
 class Reports {
 private:
     Database* db;
-    void displayTableHeader(const string& title);
-    void pressEnterToContinue();
-
-    // New Helpers
-    string getMonthName(int monthNumber);
-    void displayIntegratedChart(const vector<pair<string, int>>& data, const string& labelHeader);
 
 public:
     Reports(Database* database);
     void showMenu();
 
-    // Prescription Reports Only (Trend Chart integrated into these)
-    void generatePrescriptionReport();
-    void generateMonthlyPrescriptionReport();
-    void generateYearlyPrescriptionReport();
+    // Core Report Functions
+    void generateMonthlyReport();
+    void generateYearlyReport();
+
+    // Helper Functions
+    void displayTableHeader(const string& title);
+    void pressEnterToContinue();
 };
 
 #endif
