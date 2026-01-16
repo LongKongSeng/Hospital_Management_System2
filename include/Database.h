3dee477a2131251b031308ca0492f83159597a32
LongKongSeng <colintehjr@gmail.com>
2026-01-16 09:09:44 +0800

    Update Database.h

--- include/Database.h
+++ include/Database.h
@@ -1,13 +1,18 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
-#include <mysql_driver.h>
-#include <mysql_connection.h>
+// --- FIX: Correct MySQL Includes & Warning Suppression ---
+#pragma warning(push)
+#pragma warning(disable : 26495) // Suppress false positive warning from MySQL library
+#include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
 #include <cppconn/prepared_statement.h>
+#pragma warning(pop)
+// ---------------------------------------------------------
+
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -34,7 +39,9 @@ public:
     sql::ResultSet* executeSelect(const string& query);
     bool executeUpdate(const string& query);
     void displayError(const string& operation);
+
+    // --- NEW HELPER: Check Active Status ---
+    bool isPatientActive(const string& patientId);
 };
 
 #endif
-
